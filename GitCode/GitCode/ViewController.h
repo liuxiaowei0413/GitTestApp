4fda0b93cc04466efc052aed1fe7104c3d4e9c41
wei <students@haihaohaihao.local>
2016-04-28 23:22:54 -0700

    xin

---
+++ GitCode/GitCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitCode
+//
+//  Created by students on 16/4/28.
+//  Copyright © 2016年 LXW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitCode/GitCode/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GitCode
+//
+//  Created by students on 16/4/28.
+//  Copyright © 2016年 LXW. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
